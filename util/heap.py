a403690f7045f13ec9245ed2c40ea9c8ad0e3f37
Brian Leahy <bdl48@cornell.edu>
2020-03-11 19:23:03 -0400

    Add util.heap and tests

---
+++ util/heap.py
@@ -0,0 +1,95 @@
+from collections import deque
+
+
+class Heap(object):
+    # FIXME add a docstring!
+
+    def __init__(self, value):
+        self.value = value
+        self.num_in_heap = 1
+        self.left_child = None
+        self.right_child = None
+
+    @classmethod
+    def create_from_iterable(cls, iterable):
+        as_iterable = iter(iterable)
+        first_value = next(as_iterable)
+        heap = cls(first_value)
+        for i in as_iterable:
+            heap.add_to_heap(i)
+        return heap
+
+    def add_to_heap(self, value):
+        if value < self.value:
+            self._bubble_down(self.value)
+            self.value = value
+        else:
+            self._bubble_down(value)
+        self.num_in_heap += 1
+
+    def pop_min(self):
+        if self.num_in_heap == 0:
+            raise EmptyHeapError
+        return self._pop_min()
+
+    def _pop_min(self):
+        out = self.value
+        self.num_in_heap -= 1
+        self.value = self._bubble_up()
+        return out
+
+    def _bubble_down(self, value):  # needs a better name
+        if self.left_child is None:
+            self.left_child = self.__class__(value)
+        elif self.right_child is None:
+            self.right_child = self.__class__(value)
+        elif self.left_child.num_in_heap < self.right_child.num_in_heap:
+            self.left_child.add_to_heap(value)
+        else:
+            self.right_child.add_to_heap(value)
+
+    def _bubble_up(self):
+        # 5 options: both none, 1 none (x2), neither noen
+        if self.left_child is None and self.right_child is None:
+            out = None
+        elif self.left_child is None:
+            out = self._bubble_up_right()
+        elif self.right_child is None:
+            out = self._bubble_up_left()
+        elif self.left_child.value < self.right_child.value:
+            out = self._bubble_up_left()
+        else:
+            out = self._bubble_up_right()
+        return out
+
+    def _bubble_up_left(self):
+        out = self.left_child.pop_min()
+        if len(self.left_child) == 0:
+            self.left_child = None
+        return out
+
+    def _bubble_up_right(self):
+        out = self.right_child.pop_min()
+        if len(self.right_child) == 0:
+            self.right_child = None
+        return out
+
+    def __len__(self):
+        return self.num_in_heap
+
+
+class EmptyHeapError(Exception):
+    pass
+
+
+def heapsort(x):
+    iterable = iter(x)
+    heap = Heap(next(iterable))
+    for i in iterable:
+        heap.add_to_heap(i)
+
+    out = deque()
+    while len(heap) > 0:
+        out.append(heap.pop_min())
+    return list(out)
+
---
+++ util/test_heap.py
@@ -0,0 +1,116 @@
+import random
+import unittest
+
+from globaloptimization.util.heap import Heap, EmptyHeapError, heapsort
+
+
+class TestHeap(unittest.TestCase):
+    def test_initializes_to_zero_num_in_heap(self):
+        heap = Heap(None)
+        self.assertEqual(heap.num_in_heap, 1)
+
+    def test_initializes_child_heaps_to_none(self):
+        heap = Heap(None)
+        self.assertIs(heap.left_child, None)
+        self.assertIs(heap.right_child, None)
+
+    def test_stores_value(self):
+        value = 1755
+        heap = Heap(value)
+        self.assertIs(heap.value, value)
+
+    def test__bubble_down_creates_new_heap(self):
+        initial = 1755
+        heap = Heap(initial)
+
+        values_to_add = [1766, 1777]
+        for value_to_add in values_to_add:
+            heap._bubble_down(value_to_add)
+
+        children = [heap.left_child, heap.right_child]
+        for child, value in zip(children, values_to_add):
+            self.assertIsInstance(child, Heap)
+            self.assertIs(child.value, value)
+
+    def test_add_to_heap_keeps_ordered_when_added_low_to_high(self):
+        low = 0
+        high = 1
+        heap = Heap(low)
+        heap.add_to_heap(high)
+        self.assertEqual(heap.value, low)
+
+    def test_add_to_heap_keeps_ordered_when_added_high_to_low(self):
+        low = 0
+        high = 1
+        heap = Heap(high)
+        heap.add_to_heap(low)
+        self.assertEqual(heap.value, low)
+
+    def test_add_to_heap_keeps_count(self):
+        heap = Heap(0)
+        number_total = 10
+        for i in range(1, number_total):
+            heap.add_to_heap(number_total)
+
+        self.assertEqual(heap.num_in_heap, number_total)
+
+    def test_add_to_heap_keeps_balanced(self):
+        heap = Heap(0)
+        number_total = 25  # needs to be odd
+        for i in range(1, number_total):
+            heap.add_to_heap(number_total)
+
+        number_per_child = (number_total - 1) / 2
+        # 1 in main heap, (n - 1) / 2 in sub-heaps, for n total
+        self.assertEqual(heap.left_child.num_in_heap, number_per_child)
+        self.assertEqual(heap.right_child.num_in_heap, number_per_child)
+
+    def test_create_from_iterable_returns_correct_length(self):
+        values = [i for i in range(18)]
+        heap = Heap.create_from_iterable(values)
+        self.assertEqual(heap.num_in_heap, len(values))
+
+    def test_pop_min_returns_min_value(self):
+        values = [2, 5, 9, 2, 5, 3, 0, 1, 2]  # random integers on [0, 10]
+        heap = Heap.create_from_iterable(values)
+        out = heap.pop_min()
+        self.assertEqual(out, min(values))
+
+    def test_pop_min_decrements_num_in_heap(self):
+        num_total = 10
+        heap = Heap.create_from_iterable(range(num_total))
+        _ = heap.pop_min()
+        self.assertEqual(heap.num_in_heap, num_total - 1)
+
+    def test_len(self):
+        num_total = 10
+        heap = Heap.create_from_iterable(range(num_total))
+        self.assertEqual(len(heap), heap.num_in_heap)
+
+    def test_pop_min_bubbles_up_next(self):
+        values = sorted([1, 2, 3])
+        heap = Heap.create_from_iterable(values)
+        first = heap.pop_min()
+        second = heap.pop_min()
+        third = heap.pop_min()
+        popped = [first, second, third]
+        self.assertEqual(values, popped)
+
+    def test_pop_min_on_empty_heap_raises_error(self):
+        heap = Heap(1)
+        _ = heap.pop_min()  # heap is now empty
+        self.assertRaises(EmptyHeapError, heap.pop_min)
+
+
+class TestHeapsort(unittest.TestCase):
+    def test_heapsort(self):
+        numbers = [i for i in range(1918)]
+        random.seed(1919)
+        random.shuffle(numbers)
+
+        heap_sorted = heapsort(numbers)
+        reg_sorted = sorted(numbers)
+        self.assertEqual(heap_sorted, reg_sorted)
+
+if __name__ == '__main__':
+    unittest.main()
