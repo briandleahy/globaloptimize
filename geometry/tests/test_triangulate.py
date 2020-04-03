11edaa58c1104f381b8d3e0187f5ca1c298daa15
Brian Leahy <bdl48@cornell.edu>
2020-04-03 16:33:41 -0400

    Add triangulate_function_on_hyperrectangle

--- geometry/tests/test_triangulate.py
+++ geometry/tests/test_triangulate.py
@@ -3,11 +3,10 @@ import unittest
 import numpy as np
 
 from globaloptimize.geometry.simplex import Simplex, FunctionPoint
-from globaloptimize.geometry.triangulate import (
-    triangulate_function_points_into_simplices)
+from globaloptimize.geometry import triangulate
 
 
-class TestMisc(unittest.TestCase):
+class TestTriangulate(unittest.TestCase):
     def test_triangulate_function_points_returns_simplices(self):
         np.random.seed(1400)
         npoints = 25
@@ -16,7 +15,8 @@ class TestMisc(unittest.TestCase):
         values = np.random.randn(npoints)
         function_points = [FunctionPoint(p, v) for p, v in zip(points, values)]
 
-        simplices = triangulate_function_points_into_simplices(function_points)
+        simplices = triangulate.triangulate_function_points_into_simplices(
+            function_points)
         for simplex in simplices:
             self.assertIsInstance(simplex, Simplex)
 
@@ -28,13 +28,67 @@ class TestMisc(unittest.TestCase):
         values = np.random.randn(npoints)
         function_points = [FunctionPoint(p, v) for p, v in zip(points, values)]
 
-        simplices = triangulate_function_points_into_simplices(function_points)
+        simplices = triangulate.triangulate_function_points_into_simplices(
+            function_points)
 
         for function_point in function_points:
             point_in_simplex_i = [
                 function_point in s.function_points
                 for s in simplices]
             self.assertTrue(any(point_in_simplex_i))
+
+
+class TestHyperRectangleTriangulation(unittest.TestCase):
+    def test_produce_points_at_corners_of_hyperrectangle_2d(self):
+        bounds = [[1, 2], [3, 4]]
+
+        calculated = triangulate._produce_points_at_corners_of_hyperrectangle(
+            bounds)
+        correct = np.array([
+            [bounds[0][0], bounds[1][0]],
+            [bounds[0][0], bounds[1][1]],
+            [bounds[0][1], bounds[1][0]],
+            [bounds[0][1], bounds[1][1]]])
+
+        self.assertTrue(np.all(calculated == correct))
+
+    def test_produce_points_at_corners_of_hyperrectangle_correct_number(self):
+        for ndim in range(1, 9):
+            bounds = [[0, 1]] * ndim
+            number_of_corners = 2**ndim
+            out = triangulate._produce_points_at_corners_of_hyperrectangle(
+                bounds)
+            self.assertEqual(len(out), number_of_corners)
+
+    def test_returntype_of_triangulate_function_on_hyperrectangle(self):
+        f = lambda x: np.cos(x).sum()
+        np.random.seed(947)
+        bounds = np.random.randn(4, 2)
+
+        triangulation = triangulate.triangulate_function_on_hyperrectangle(
+            f, bounds)
+        for simplex in triangulation:
+            self.assertIsInstance(simplex, Simplex)
+            for function_point in simplex.function_points:
+                self.assertIsInstance(function_point, FunctionPoint)
+                self.assertEqual(function_point.value, f(function_point.point))
+
+    def test_triangulation_of_triangulate_function_on_hyperrectangle(self):
+        bounds = np.array([[0, 1], [0, 1]])
+        f = lambda x: np.sum(x)
+        triangulation = triangulate.triangulate_function_on_hyperrectangle(
+            f, bounds)
+
+        points = np.array([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]])
+        correct_function_points = set([FunctionPoint(p, f(p)) for p in points])
+        calculated_function_points = set(
+            [fp for s in triangulation for fp in s.function_points])
+
+        self.assertEqual(calculated_function_points, correct_function_points)
 
 
 if __name__ == '__main__':
--- geometry/triangulate.py
+++ geometry/triangulate.py
@@ -4,6 +4,13 @@ from scipy.spatial import Delaunay
 from globaloptimize.geometry.simplex import Simplex, FunctionPoint
 
 
+def triangulate_function_on_hyperrectangle(function, bounds):
+    points = _produce_points_at_corners_of_hyperrectangle(bounds)
+    function_points = [FunctionPoint(point, function(point))
+                       for point in points]
+    return triangulate_function_points_into_simplices(function_points)
+
+
 def triangulate_function_points_into_simplices(function_points):
     points = [fp.point for fp in function_points]
     triangulation = Delaunay(np.asarray(points))
@@ -12,4 +19,26 @@ def triangulate_function_points_into_simplices(function_points):
         these_points = [function_points[i] for i in indices]
         simplices.append(Simplex(these_points))
     return simplices
+
+
+def _produce_points_at_corners_of_hyperrectangle(bounds):
+    """
+    Parameters
+    ----------
+    bounds : (N, 2) list-like of bounds for each parameter
+    """
+    ndim = len(bounds)
+    origin = np.array([b[0] for b in bounds])
+    side_lengths = np.array([b[1] - b[0] for b in bounds])
+    sides = np.eye(ndim, dtype='float') * side_lengths.reshape(ndim, 1)
+
+    corners = []
+    for corner_id in range(2**ndim):
+        # Get a binary encoding for the corner as a string, '00001' etc
+        encoding = bin(corner_id).split('b')[1].rjust(ndim, '0')
+        this_corner = origin.copy().astype('float')
+        for side, zero_or_one in zip(sides, encoding):
+            this_corner += side * int(zero_or_one)
+        corners.append(this_corner)
+    return np.array(corners)
 
